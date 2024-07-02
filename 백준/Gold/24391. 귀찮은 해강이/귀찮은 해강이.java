ba7f3ae31649ad4593c845aa74d3eddc8d31370b
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-07-02 15:51:57 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 448 ms, Memory: 51140 KB -BaekjoonHub

---
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] parent, input;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine(), " ");
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+
+        parent=new int[N+1];
+        input=new int[N+1];
+
+        for(int i=1; i<=N; i++){
+            parent[i]=i;
+        }
+
+        for(int i=0; i<M; i++){
+            st=new StringTokenizer(br.readLine(), " ");
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+       
+        st=new StringTokenizer(br.readLine(), " ");
+        for(int i=1; i<=N; i++){
+            input[i]=Integer.parseInt(st.nextToken());
+        }
+
+        int count=0;
+        for (int i=2; i<=N; i++) {
+            if (find(input[i]) != find(input[i-1])) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static boolean union(int i, int j) {
+        int iRoot=find(i);
+        int jRoot=find(j);
+        if(iRoot==jRoot) return false;
+        parent[jRoot]=iRoot;
+        return true;
+    }
+
+    static int find(int k) {
+        if (parent[k] == k) return k;
+        return parent[k] = find(parent[k]);
+    }
+}
