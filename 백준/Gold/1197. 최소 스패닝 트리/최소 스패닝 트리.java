1a5433a58bdb760cc94ac4b25f342399631e129b
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-07-03 15:37:10 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 468 ms, Memory: 53472 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 
+/*
+* PriorityQueue 버전
+*/
 public class Main {
 	
 	public static void main(String[] args)throws Exception {
@@ -10,7 +13,7 @@ public class Main {
 		int N=Integer.parseInt(st.nextToken());
 		int E=Integer.parseInt(st.nextToken());
 		
-		List<int[]>[] g=new List[N];for(int i=0;i<N;i++) g[i]=new ArrayList<>(); // 정점,cost
+		List<int[]>[] g=new List[N];for(int i=0;i<N;i++) g[i]=new ArrayList<>(); //정점,cost
 		
 		for(int i=0;i<E;i++) {
 			st=new StringTokenizer(br.readLine()," ");
@@ -23,34 +26,36 @@ public class Main {
 	
 		boolean[] v=new boolean[N];
 		
-		int[]w=new int[N];
+		int[]w=new int[N];	//최소 비용(가중치) 저장
 		for(int i=0;i<N;i++) {
 			w[i]=Integer.MAX_VALUE;
 		}
 			
 		PriorityQueue<int[]> pq=new PriorityQueue<>((o1,o2)->Integer.compare(o1[1], o2[1]));
 		int sum=0,cnt=0;
-		w[0]=0;
+		w[0]=0;		//출발하고자 하는 정점, 방문처리
 		pq.offer(new int[] {0,0});
 		
+		//prim에서 최소비용을 찾는 STEP1 과정 생략
 		while(!pq.isEmpty()) {
 			int []vc=pq.poll();
 			int min=vc[1];
 			int minVertex=vc[0];
 			if(v[minVertex])continue;
 			
-			v[minVertex]=true;
-			sum+=min;
+			//STEP2
+			v[minVertex]=true;		//방문처리
+			sum+=min;	//스패닝트리에 누적
 			if(cnt++==N-1)break;
 			
+			
+			//STEP3: w배열 최소비용값을 갱신
 			for(int[] j:g[minVertex]) {
 				if(!v[j[0]] && w[j[0]]>j[1]) {
 							   w[j[0]]=j[1];
-							   pq.offer(j);
-					
+							   pq.offer(j);		
 				}
 			}
-//			System.out.println();
 		}
 	
 		System.out.println(sum);
