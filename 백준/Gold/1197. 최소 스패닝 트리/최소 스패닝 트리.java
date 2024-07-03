317143cd3bdeba9cc187001c1629ab7c3d59ca14
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-07-03 13:20:40 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 828 ms, Memory: 53028 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,63 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;	//정점의 개수
-	static int[][] g;	//간선 정보 저장 배열
-	static int[] p;		//부모 배열
-	
-	static void make() {
-		p=new int[N+1];
-		for(int i=0;i<N+1;i++)p[i]=i;	
-	}
-	
-	static int find(int a) {	
-		if(p[a]==a) return a;
-		return p[a]=find(p[a]);
-	}
-	
-	static boolean union(int a,int b) { 
-		int aRoot=find(a);
-		int bRoot=find(b);
-		if(aRoot==bRoot) return false; 	
-//		if(aRoot<bRoot) p[bRoot]=aRoot;
-//		else p[aRoot]=bRoot;
-		p[bRoot]=aRoot;
-		return true;
-
-	}
 	
 	public static void main(String[] args)throws Exception {
-		Scanner sc=new Scanner(System.in);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine()," ");
 		
-		N=sc.nextInt();
-		int E=sc.nextInt();
-//		System.out.println(N+","+ E);
+		int N=Integer.parseInt(st.nextToken());
+		int E=Integer.parseInt(st.nextToken());
 		
-		g=new int[E][3];
+		List<int[]>[] g=new List[N];for(int i=0;i<N;i++) g[i]=new ArrayList<>(); // 정점,cost
 		
 		for(int i=0;i<E;i++) {
-			int from=sc.nextInt();
-			int to=sc.nextInt();
-			int cost=sc.nextInt();
-			g[i]=new int[] {from,to,cost};
-			
+			st=new StringTokenizer(br.readLine()," ");
+			int from=Integer.parseInt(st.nextToken());
+			int to=Integer.parseInt(st.nextToken());
+			int cost=Integer.parseInt(st.nextToken());
+			g[from-1].add(new int[] {to-1,cost});
+			g[to-1].add(new int[] {from-1,cost});
 		}
-//		for(int[] e:g)System.out.println(Arrays.toString(e));System.out.println();
-		Arrays.sort(g,(o1,o2)->Integer.compare(o1[2], o2[2]));	//기본데이터 타입인 경우
-//		for(int[] e:g)System.out.println(Arrays.toString(e));System.out.println();
-//		Arrays.sort(g,(o1,o2)->o1[2].compareTo(o2[2]));			//기본데이터 타입 아닌 경우 
+	
+		boolean[] v=new boolean[N];
 		
-		make();
-		int sum=0, cnt=0;
-		for(int[] e:g) {
-			if(union(e[0],e[1])) {
-				sum+=e[2];
-				if(++cnt==N-1) break;		// ++cnt 하는 이유:
+		int[]w=new int[N];
+		for(int i=0;i<N;i++) {
+			w[i]=Integer.MAX_VALUE;
+		}
+		
+		int sum=0,cnt=0;
+		w[0]=0;
+//		System.out.println(Arrays.toString(w));
+		
+		for(int i=0;i<N;i++) {
+			int min=Integer.MAX_VALUE;
+			int minVertex=-1;
+			for(int j=0;j<N;j++) {
+				if(!v[j] && min>w[j]) {
+					min=w[j];
+					minVertex=j;
+				}
 			}
+			
+			v[minVertex]=true;
+			sum+=min;
+			if(cnt++==N-1)break;
+			
+			for(int[] j:g[minVertex]) {
+				if(!v[j[0]] && w[j[0]]>j[1]) {
+							   w[j[0]]=j[1];
+					
+				}
+			}
+//			System.out.println();
 		}
+	
 		System.out.println(sum);
-		
-		sc.close();
+		br.close();
 	}
 
 }
