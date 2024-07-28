9caa6eab29a8c0ed747193545b0b17a9f50509a3
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-07-28 19:27:43 +0900

    [Silver II] Title: 최대 힙, Time: 332 ms, Memory: 26264 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> que = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0)
+                sb.append(que.size() == 0 ? 0 : que.poll()).append('\n');
+            else que.add(num);
+        }
+        System.out.println(sb.toString());
+    }
+
+}
