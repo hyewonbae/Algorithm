f431c22a54dadff4a210e369378c95854219e761
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-08-09 20:42:49 +0900

    [Bronze III] Title: 홀수, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int minNum = 0;
+		for (int i = 0; i < 7; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num % 2 == 1) {
+				if (minNum == 0)
+					minNum = num;
+				if (minNum > num) {
+					minNum = num;
+				}
+				sum += num;
+			}
+		}
+		if (sum == 0) {
+			System.out.println(-1);
+			return;
+		}
+		System.out.println(sum);
+		System.out.println(minNum);
+	}
+}
