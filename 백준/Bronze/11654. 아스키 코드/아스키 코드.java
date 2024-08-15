cc15e9f20b1f20d3393b7ef0ed735b2e6558aad6
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-08-15 21:45:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        int n = c;
+        System.out.println(n);
+    }
+}
