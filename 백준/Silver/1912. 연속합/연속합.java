90a72c6d5e2f15a0b9bda6cd61e29a4be25e35d1
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-09-07 13:27:58 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 23768 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 10
+ * 10 -4 3 1 5 6 -35 12 21 -1
+ *
+ * 33
+ * */
+public class Main {
+    static int n;
+    static int[] arr;
+    static int[] dp;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        dp = new int[n];
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = arr[0];
+        max = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
