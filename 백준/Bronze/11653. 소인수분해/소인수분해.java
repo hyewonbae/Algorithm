1b854941ef3d8dcfc47fa8d075a1539ab7c72771
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-09-22 18:25:16 +0900

    [Bronze I] Title: 소인수분해, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                sb.append(i).append('\n');
+                n /= i;
+            }
+        }
+        if (n != 1) {
+            sb.append(n);
+        }
+        System.out.println(sb);
+    }
+}
