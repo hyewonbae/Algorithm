6f6b16481ca3f564084e5a56fbea520c037396f9
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2025-01-20 23:58:28 +0900

    [Silver II] Title: 돌 게임 4, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+/**
+6
+
+SK
+*/
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 2;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 1;
+        for (int i = 5; i <= n; i++) {
+            dp[i] = 2;
+            if (dp[i-1] == 2 || dp[i-3] == 2 || dp[i-4] == 2) dp[i] = 1;
+        }
+        if (dp[n] == 1) {
+            System.out.print("SK");
+        } else {
+            System.out.print("CY");
+        }
+    }
+}
