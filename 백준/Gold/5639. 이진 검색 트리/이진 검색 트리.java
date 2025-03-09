14bcae9731bc955e16c3bec13eed90c8d442ea15
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2025-03-09 20:41:38 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 624 ms, Memory: 19612 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+/**
+ * 50
+ * 30
+ * 24
+ * 5
+ * 28
+ * 45
+ * 98
+ * 52
+ * 60
+ *
+ * 5
+ * 28
+ * 24
+ * 45
+ * 30
+ * 60
+ * 52
+ * 98
+ * 50
+ * */
+public class Main {
+    static class Node {
+        int num;
+        Node left, right;
+
+        Node(int num) {
+            this.num = num;
+        }
+
+        Node(int num, Node left, Node right) {
+            this.num = num;
+            this.left = left;
+            this.right = right;
+        }
+        void insert(int n) {
+            if(n < this.num){
+                if(this.left == null){
+                    this.left = new Node(n);
+                }else{
+                    this.left.insert(n);
+                }
+            }else{
+                if(this.right == null){
+                    this.right = new Node(n);
+                }else{
+                    this.right.insert(n);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        String input;
+
+        while(true){
+            input = br.readLine();
+            if(input == null || input.equals("")){
+                break;
+            }
+            root.insert(Integer.parseInt(input));
+        }
+
+        postOrder(root);
+    }
+    static void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.num);
+    }
+}
