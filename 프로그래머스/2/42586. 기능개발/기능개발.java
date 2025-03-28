24816db6b4b94e56c58f878de3d927ff3be80cd8
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2025-03-28 14:35:46 +0900

    [level 2] Title: 기능개발, Time: 0.42 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,46 +1,27 @@
-/**
- * 각 기능은 진도가 100% 일 때 서비스에 반영할 수 있음.
- * 뒤에 있는 기능이 먼저 개발되어도,
- * 앞에 있는 기능이 100%가 되어 배포될 때 반영할 수 있음.
- * 배포는 하루에 한 번만 수행
- *
- * 작업 진도 < 100
- * 작업 속도 <= 100
- * return 배포마다 몇 개의 기능이 배포되는지
- * */
-
-import java.util.*;
+import  java.util.*;
 
 class Solution {
     public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
 
-        // 각 기능이 완료되기까지 필요한 작업일수 계산
+        // 각 기능의 배포까지 걸리는 작업일 계산
         for (int i = 0; i < progresses.length; i++) {
-            int work_days = (100-progresses[i]) % speeds[i];
-            if(work_days == 0){
-                queue.offer((100-progresses[i]) / speeds[i]);
-            } else {
-                queue.offer(((100-progresses[i]) / speeds[i]) + 1);
-            }
+            int workdays = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            queue.add(workdays);
         }
 
-//        System.out.println(queue);  // queue = [7, 3, 9]
-
-        ArrayList<Integer> answer = new ArrayList<>();
-        while(!queue.isEmpty()){
-            int first_work_day = queue.poll();
-            int count = 1;
-
-            // 다음 기능들이 첫번째 작업 일수보다 작거나 같은 경우 함께 배포
-            while(!queue.isEmpty() && first_work_day >= queue.peek()){
+        while (!queue.isEmpty()) {
+            int deployDay = queue.poll();  // 가장 앞의 작업 배포 기준
+            int count = 1;  // 최소 1개의 작업은 배포됨
+            // 이후 작업들 중에서 배포 가능하면 함께 배포
+            while (!queue.isEmpty() && queue.peek() <= deployDay) {
                 queue.poll();
                 count++;
             }
-
-            // 배포될 기능의 개수를 결과에 추가
-            answer.add(count);
+            result.add(count);
         }
-        return answer;
+
+        return result;
     }
 }
