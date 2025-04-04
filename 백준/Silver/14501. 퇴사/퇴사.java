2506c9c891396f6b15b10be61e7f369ebcfa4409
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2025-04-05 01:38:49 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,38 +1,34 @@
-/**
- * 7
- * 3 10
- * 5 20
- * 1 10
- * 1 20
- * 2 15
- * 4 40
- * 2 200
- *
- * 45
- * */
-
 import java.io.*;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int Case = sc.nextInt();
-        int[] T = new int[Case];//소요 기간
-        int[] P = new int[Case];//금액
-        for(int i=0;i<Case;i++) {
-            T[i]=sc.nextInt();
-            P[i]=sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] t = new int[n];
+        int[] p = new int[n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
         }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+//        for(int i = n-1; i >= 0; i--){
+//            if(i+t[i] <= n){
+//                dp[i] = Math.max(dp[i+1], p[i]+dp[i+t[i]]);
+//            } else {
+//                dp[i] = dp[i+1];
+//            }
+//        }
+//        System.out.println(dp[0]);
 
-        int[] dp = new int[Case+1];
-        for(int i=0;i<Case;i++) {
-            if(i+T[i]<=Case) {	//범위에 벗어나지 않는다면
-                dp[i+T[i]]=Math.max(dp[i+T[i]],dp[i]+P[i]);
+        for(int i = 0; i < n; i++){
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+            if(i+t[i] <= n){
+                dp[i+t[i]] = Math.max(dp[i+t[i]], dp[i] + p[i]);
             }
-            dp[i+1]=Math.max(dp[i+1],dp[i]);	//다음dp = 현재 누적값 vs 다음 누적값
-
         }
-        System.out.println(dp[Case]);
-
+        System.out.println(dp[n]);
     }
 }
